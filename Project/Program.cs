09c7952469b0ef9acd9d5a97ecbd47104432e128
Sergey <isergeytarhov@gmail.com>
2022-11-23 18:19:49 +0300

    add FillArray Method

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+string[] array = {"the", "some", "word", "32"};
+string[] newarray = new string[array.Length];
